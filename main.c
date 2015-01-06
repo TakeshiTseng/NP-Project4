7945101bd9a5f816bb706d69476488410893a7d4
Takeshi <a86487817@gmail.com>
2015-01-06 16:18:42 +0800

    finish binding mode, but there has bug

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <time.h>
-#include "passivesock.h"
 
 typedef unsigned char byte_t;
 struct sock4pkt {
@@ -73,12 +72,11 @@ void handleSIGCHLD() {
     // while(wait3(&stat, WNOHANG, (struct rusage*)0)>=0);
 }
 
-void create_server_sock(int port) {
-    struct sockaddr_in sock_server;
-    sock_server.sin_family = AF_INET;
-    sock_server.sin_addr.s_addr = INADDR_ANY;
-    sock_server.sin_port = htons(port);
-    memset((coid*)&(sock_server.sin_zero), 0, 8);
+int create_server_sock(int port, struct sockaddr_in* sock_server) {
+    sock_server->sin_family = AF_INET;
+    sock_server->sin_addr.s_addr = INADDR_ANY;
+    sock_server->sin_port = htons(port);
+    memset((void*)&(sock_server->sin_zero), 0, 8);
 
     return socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 }
@@ -89,13 +87,24 @@ int main(int argc, const char *argv[])
     signal(SIGCHLD, handleSIGCHLD);
     int c;
     srand(time(NULL));
-    int port = 2000 + rand() % 100;
+    int port = 8000 + rand() % 100;
     printf("Port : %d\n", port);
 
-    int sc_fd = create_server_sock(port);
+    struct sockaddr_in sock_server;
+    int sc_fd = create_server_sock(port, &sock_server);
 
     if(sc_fd < 0) {
         perror("Create sock server error");
+        return -1;
+    }
+
+    if(bind(sc_fd, (const struct sockaddr*)&sock_server, sizeof(struct sockaddr)) < 0) {
+        perror("Bind error");
+        return -1;
+    }
+
+    if(listen(sc_fd, 10) < 0) {
+        perror("Listen error");
         return -1;
     }
 
@@ -105,6 +114,10 @@ int main(int argc, const char *argv[])
 
     while(1 == 1){
         int client_sock = accept(sc_fd, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen);
+        if(client_sock < 0) {
+            perror("accept error");
+            break;
+        }
         int pid = fork();
 
         if(pid == 0){
@@ -142,7 +155,7 @@ int main(int argc, const char *argv[])
                         memcpy(&rfds, &afds, sizeof(rfds));
 
                         if(select(nfds, &rfds, NULL, NULL, NULL) < 0) {
-                            perror("select error");
+                            perror("[Conn]select error");
                             fflush(stdout);
                             //close(sock);
                             close(client_sock);
@@ -152,14 +165,14 @@ int main(int argc, const char *argv[])
                         if(FD_ISSET(sock, &rfds)) {
                             int len = read(sock, buffer, 8192);
                             if(len > 0){
-                                printf("Data read from sock: %d\n", len);
+                                printf("[Conn]Data read from sock: %d\n", len);
                                 fflush(stdout);
                                 write(client_sock, buffer, len);
                             }
                         } else if(FD_ISSET(client_sock, &rfds)) {
                             int len = read(client_sock, buffer, 8192);
                             if(len > 0){
-                                printf("Data read from client sock: %d\n", len);
+                                printf("[Conn]Data read from client sock: %d\n", len);
                                 fflush(stdout);
                                 write(sock, buffer, len);
                             }
@@ -174,24 +187,51 @@ int main(int argc, const char *argv[])
                 exit(0);
             } else if(pkt.cd == 2) {
                 // Bind mode! Let's rock!!!
-
-                int bind_sc_fd; // TODO
+                printf("[Bind] Bind mode\n");
+                // for bind server
                 struct sockaddr_in server;
+                printf("[Bind] Createing server sock\n");
+                port = 9000 + rand() % 100;
+                int bind_sc_fd = create_server_sock(port, &server);
+
+                printf("[Bind] Bind server port: %d\n", port);
+
+                printf("[Bind] Binding\n");
+                if(bind(bind_sc_fd, (const struct sockaddr*)&server, sizeof(struct sockaddr)) < 0) {
+                    perror("[Bind] Bind error");
+                    sock_reply(client_sock, pkt, 0);
+                    return -1;
+                }
+
+                printf("[Bind] Listening\n");
+                if(listen(sc_fd, 10) < 0) {
+                    perror("[Bind] Listen error");
+                    sock_reply(client_sock, pkt, 0);
+                    return -1;
+                }
+
+                printf("[Bind] Connecting to dest server\n");
+                printf("[Bind] dst ip: %d, port: %d\n", pkt.dst_ip, pkt.dst_port);
+                // to connect server
                 int sock = socket(AF_INET , SOCK_STREAM , 0);
                 server.sin_addr.s_addr = pkt.dst_ip;
                 server.sin_family = AF_INET;
                 server.sin_port = htons(pkt.dst_port);
-                if(connect(sock, (struct sockaddr *)&server , sizeof(server)) < 0) {
-                    perror("[bind]connect error");
+
+                if(bind_sc_fd < 0) {
+                    perror("[Bind] Create error\n");
+                    sock_reply(client_sock, pkt, 0);
+                } else if(connect(sock, (struct sockaddr *)&server , sizeof(server)) < 0) {
+                    perror("[bind] Connect error");
                     sock_reply(client_sock, pkt, 0);
                 } else {
                     pkt.dst_ip = 0;
-                    pkt.dst_port = 0; // TODO
+                    pkt.dst_port = server.sin_port;
                     sock_reply(client_sock, pkt, 1);
 
-                    printf("Wait for bind client.....\n");
+                    printf("[Bind] accepting.....\n");
                     int bind_client_sock = accept(bind_sc_fd, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen);
-                    printf("OK, Bind!\n");
+                    printf("[Bind] OK, Accept!\n");
                     sock_reply(client_sock, pkt, 1);
 
                     fd_set rfds, afds;
@@ -199,9 +239,35 @@ int main(int argc, const char *argv[])
                     FD_ZERO(&afds);
                     FD_SET(sock, &afds);
                     FD_SET(bind_client_sock, &afds);
+                    while(1 == 1) {
 
+                        memcpy(&rfds, &afds, sizeof(rfds));
 
-                    close(bind_client_sock);
+                        if(select(nfds, &rfds, NULL, NULL, NULL) < 0) {
+                            perror("select error");
+                            fflush(stdout);
+                            //close(sock);
+                            close(bind_client_sock);
+                            break;
+                        }
+                        bzero(buffer, 8192);
+                        if(FD_ISSET(sock, &rfds)) {
+                            int len = read(sock, buffer, 8192);
+                            if(len > 0){
+                                printf("[Bind] Data read from sock: %d\n", len);
+                                fflush(stdout);
+                                write(bind_client_sock, buffer, len);
+                            }
+                        } else if(FD_ISSET(bind_client_sock, &rfds)) {
+                            int len = read(bind_client_sock, buffer, 8192);
+                            if(len > 0){
+                                printf("[Bind] Data read from client sock: %d\n", len);
+                                fflush(stdout);
+                                write(sock, buffer, len);
+                            }
+                        }
+
+                    }
                 }
                 close(bind_sc_fd);
             }
